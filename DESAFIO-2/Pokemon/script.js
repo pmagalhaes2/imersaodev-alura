39765a2553d7b33966c3c1072ce16cabf5363f64
Patricia Magalhães <paath.magalhaes2@gmail.com>
2021-03-25 23:40:43 -0300

    updating

---
+++ DESAFIO-2/Pokemon/script.js
@@ -0,0 +1,6 @@
+var resposta = prompt("Quem é esse Pokémon?")
+if (resposta == "pikachu" || resposta == "Pikachu" || resposta =="PIKACHU") {
+  document.write("<h2><font color='green'>Você acertou!</font></h2>")
+} else {
+  document.write("<h2><font color='red'>Você errou!</font></h2>")
+}
